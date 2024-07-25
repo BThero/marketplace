e7df96568b205286a03ac156eeda911b397215e2
Temirlan Baibolov <bthero09@gmail.com>
2024-07-26 06:28:09 +0700

    feat: implement simple home page

---
+++ src/app/(app)/actions.ts
@@ -0,0 +1,36 @@
+'use server';
+
+import { z } from 'zod';
+import { createServerAction } from 'zsa';
+import { db } from '../../db/drizzle';
+import { item as itemTable } from '../../db/schema';
+import { eq } from 'drizzle-orm';
+import { revalidatePath } from 'next/cache';
+
+const schema = z.object({
+  id: z.number(),
+});
+
+export const claimItem = createServerAction()
+  .input(schema)
+  .handler(async ({ input }) => {
+    const { id } = input;
+    await db.transaction(async (tx) => {
+      const item = await tx.query.item.findFirst({
+        where(fields, operators) {
+          return operators.eq(fields.id, id);
+        },
+      });
+      if (!item || item.isClaimed === true) {
+        throw new Error('Item not found or already claimed');
+      }
+      await tx
+        .update(itemTable)
+        .set({
+          isClaimed: true,
+        })
+        .where(eq(itemTable.id, id));
+    });
+    revalidatePath('/');
+    return true;
+  });
--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,8 +1,53 @@
+import { db } from '../../db/drizzle';
+import { readdirSync } from 'fs';
+import { item as itemTable } from '../../db/schema';
+import assert from 'assert';
+import { Item } from './Item';
+import { Metadata } from 'next';
+
+const capitalize = (text: string): string => {
+  return text.charAt(0).toUpperCase() + text.slice(1);
+};
+
+const populateItems = async () => {
+  const files = readdirSync('public/items');
+  await db.delete(itemTable);
+
+  for (const file of files) {
+    assert(file.endsWith('.jpg'), 'All items should be in .jpg format');
+    await db.insert(itemTable).values({
+      imageUrl: `/items/${file}`,
+      title: capitalize(
+        file.substring(0, file.length - 4).replaceAll('_', ' ')
+      ),
+    });
+  }
+};
+
+export const metadata: Metadata = {
+  title: "Tim's Marketplace",
+};
+
 const Home = async () => {
+  // await populateItems();
+  const items = await db.query.item.findMany({
+    orderBy: [itemTable.id],
+  });
   return (
-    <div className="flex-1 w-full flex justify-center pt-12">
-      <span className="typography-h1">Home</span>
-    </div>
+    <main className="flex-1 w-full flex flex-col items-center gap-4 p-2">
+      <header>
+        <h1 className="typography-h1">Tim&apos;s Marketplace</h1>
+        <ul className="list-disc list-inside typography-list">
+          <li>All items are free</li>
+          <li>Reach out to me before August 1st if you claim anything</li>
+        </ul>
+      </header>
+      <div className="grid grid-cols-3 gap-4 w-[1000px] max-sm:grid-cols-1 max-sm:w-full max-lg:grid-cols-2 max-lg:w-full">
+        {items.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </div>
+    </main>
   );
 };
 
