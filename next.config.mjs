ca065f2e7b10c367d9972c7937d7a74343fac295
Temirlan Baibolov <bthero09@gmail.com>
2025-09-18 00:12:57 +0800

    upgrade react

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@node-rs/argon2'],
-  },
   images: {
     deviceSizes: [320, 640, 768, 1024, 1280],
     imageSizes: [16, 32, 48, 64, 96],
